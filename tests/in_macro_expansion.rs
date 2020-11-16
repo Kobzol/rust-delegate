3019a53d12a5d405523df2ee2ece2732195e184f
Jakub Beránek <berykubik@gmail.com>
2020-11-16 20:52:09 +0100

    Bump version to 0.5.0

--- tests/in_macro_expansion.rs
+++ tests/in_macro_expansion.rs
@@ -1,4 +1,4 @@
-/*///
+///
 /// Tests that a macro can expand into a delegate block,
 /// where the target comes from a macro variable.
 ///
@@ -20,21 +20,20 @@ struct Struct2(pub Struct1);
 // We use a macro to impl 'Trait' for 'Struct2' such that
 // the target is a variable in the macro.
 macro_rules! some_macro {
-    ($delegate_to:expr) => {
+    (|$self:ident| $delegate_to:expr) => {
         impl Trait for Struct2 {
             delegate! {
                 // '$delegate_to' will expand to 'self.0' before ´delegate!' is expanded.
                 to $delegate_to {
-                    fn method_to_delegate(&self) -> bool;
+                    fn method_to_delegate(&$self) -> bool;
                 }
             }
         }
     };
 }
-some_macro!(self.0);
+some_macro! { |self | self.0 }
 
 #[test]
 fn test() {
     assert!(Struct2(Struct1()).method_to_delegate());
 }
-*/
